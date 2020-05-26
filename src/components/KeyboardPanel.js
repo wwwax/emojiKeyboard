a69cc4da5d4eb47e6104ef0efef63ce97179c9d6
Max Ivanov <waxivanov@gmail.com>
2020-05-26 16:57:11 +0300

    papper-clip button

--- src/components/KeyboardPanel.js
+++ src/components/KeyboardPanel.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   TextInput,
   View,
   TouchableOpacity,
   Image,
   StyleSheet,
+  TouchableWithoutFeedback,
 } from 'react-native';
 
 export const KeyboardPanel = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button}>
-        <Image source={require('../img/emoji-button.png')} />
-      </TouchableOpacity>
+      <TouchableWithoutFeedback>
+        <TouchableOpacity style={styles.button}>
+          <Image source={require('../img/emoji-button.png')} />
+        </TouchableOpacity>
 
-      <TextInput style={styles.input} placeholder="Type your text here..." />
+        <TextInput style={styles.input} placeholder="Type your text here..." />
 
-      <TouchableOpacity style={styles.button}>
-        <Image source={require('../img/send-arrow.png')} />
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setModalVisible(true)}>
+          <Image source={require('../img/papper-clip.png')} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Image source={require('../img/send-arrow.png')} />
+        </TouchableOpacity>
+      </TouchableWithoutFeedback>
     </View>
   );
 };
@@ -32,8 +43,10 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 10,
+    backgroundColor: 'red',
   },
   input: {
     flex: 1,
+    backgroundColor: 'lightgrey',
   },
 });
